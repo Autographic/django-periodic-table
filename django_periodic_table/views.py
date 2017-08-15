8a4b265acc221213470ad1d24711445c76a10bd3
Murray Pearson <murray@autographic.ca>
2017-08-15 07:06:52 -0400

    Update views.py
    Fixed template name

--- django_periodic_table/views.py
+++ django_periodic_table/views.py
@@ -4,7 +4,7 @@ from .models import Element
 
 
 class IndexView(TemplateView):
-    template_name = "periodic_table_index.html"
+    template_name = "django_periodic_table/periodic_table_index.html"
     
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
