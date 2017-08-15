ba2dc9716d725b7fa46b104109c77e87ec8c77ec
Murray Pearson <murray@autographic.ca>
2017-08-15 06:52:46 -0400

    Update urls.py
    Simplified default URL

--- django_periodic_table/urls.py
+++ django_periodic_table/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import IndexView
 
 urlpatterns = [
-    url(r'^/', IndexView.as_view(), name="periodic_table"),
+    url(r'', IndexView.as_view(), name="periodic_table"),
 ]
